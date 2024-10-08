ad42cf0d5b06d9d664f26861cc657cc5bcae1a59
YEISON RAMIREZ BAENA <yeibaena@gmailcom>
2024-10-07 21:01:01 -0500

    v1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        //Nombrar variables //
+
+        bool Jugar = true;
+        while (Jugar)
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to the game'Adivina el numero'!");
+            Console.Write("Ingrese el numero de jugadores (2-4): ");
+            int numPlayers = int.Parse(Console.ReadLine());
+
+            int RangoMax = numPlayers switch
+            {
+                2 => 50,
+                3 => 100,
+                4 => 200,
+                _ => throw new ArgumentException("Numero de jugadotres no valido ")
+            };
+            Random random = new Random();
+            int
+
+
+                {
+
+                }
+            }
+        }
+    }
+}
