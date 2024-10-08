f9a6aede9d29afa328bdede2ce47aaad7402cf8c
Yeison Ramirez Baena <yeibaena@gmail.com>
2024-10-07 23:03:59 -0500

    Program.cs..
    v3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿internal class Program
+internal class Program
 {
     private static void Main(string[] args)
     {
@@ -21,13 +21,33 @@
                 _ => throw new ArgumentException("Numero de jugadotres no valido ")
             };
             Random random = new Random();
-            int
+            int numeroSorpresa = random.Next(0, RangoMax + 1);
 
+            bool AdivinoElNumero = false;
+            int Turno = 0;
+            while (!AdivinoElNumero)
+            {
+                Console.WriteLine($"\nTurno del jugador {Turno + 1}");
+                Console.Write("Ingrese su número: ");
+                int numeroIngresado = int.Parse(Console.ReadLine());
 
+                if (numeroIngresado > numeroSorpresa)
                 {
-
+                    Console.WriteLine("MENOR");
                 }
+                else if (numeroIngresado < numeroSorpresa)
+                {
+                    Console.WriteLine("MAYOR");
+                }
+                else
+                {
+                    Console.WriteLine("¡Ganaste!");
+                    AdivinoElNumero = true;
+                }
+                Turno = (Turno + 1) % numPlayers;
             }
+            Console.Write("\n¿Deseas Jugar De Nuevo? (s/n): ");
+            Jugar = Console.ReadLine().ToLower() == "s";
         }
     }
 }
